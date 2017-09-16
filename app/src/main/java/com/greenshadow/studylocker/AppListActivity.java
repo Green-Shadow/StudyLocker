8a4769c35c7c4357c355dd073832d4bcd54533ba
TheKarlos <kamtechie@gmail.com>
2017-09-16 19:40:40 +0530

    Initial commit

---
+++ app/src/main/java/com/greenshadow/studylocker/AppListActivity.java
@@ -0,0 +1,26 @@
+package com.greenshadow.studylocker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class AppListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_list);
+
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2" };
+        // use your custom layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.list_item_layout, R.id.appName, values);
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+    }
+    }
+}
